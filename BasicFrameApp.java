87b7a7c1b72a9bb5605234c3a9f75c676c1128bc
BathabileMkhabela <139801564+BathabileMkhabela@users.noreply.github.com>
2023-07-17 20:08:58 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,16 @@
+
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+public class BasicFrameApp {
+
+    public static void main(String[] args) {
+        //declare variables
+        MyFirstFrame myFrame;
+        
+        //Instantiate the frame
+        myFrame= new MyFirstFrame();
+    }
+    
+}
